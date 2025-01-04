6bff84c590b2d2669b00bb00b02cca6b8dffe195
chewkel <picalpical2004@gmail.com>
2025-01-04 22:42:42 +0000

    added login,logout and some other links for future tasks

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-// const redirectLogin = (req, res, next) => {
-//     if (!req.session.userId) {
-//         res.redirect("./login");
-//     } else {
-//         next();
-//     }
-// };
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect("./login");
+    } else {
+        next();
+    }
+};
 
 const { check, validationResult } = require("express-validator");
 const { get } = require("request");
@@ -26,51 +26,6 @@ router.get('/users', async (req, res) => {
         res.status(500).send('Database query failed');
     }
 });
-
-// router.post(
-//     "/registered",
-//     [
-//       check("username").isLength({ min: 3 }),
-//       check("email").isEmail(),
-//       check("password").isLength({ min: 5 }),
-//     ],
-//     function (req, res, next) {
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         return res.status(422).json({ errors: errors.array() });
-//       }
-//       let username = req.sanitize(req.body.username);
-//       let email = req.sanitize(req.body.email);
-//       let hashedPassword = req.sanitize(req.body.password);
-//       let created_at = new Date();
-//       let sqlquery = "SELECT * FROM users WHERE username = ?";
-//       db.query(sqlquery, [username], (err, result) => {
-//         if (err) {
-//           next(err);
-//         }
-//         if (result.length > 0) {
-//           res.send("Username already exists");
-//         } else {
-//           bcrypt.hash(hashedPassword, saltRounds, function (err, hashedPassword) {
-//             let sqlquery =
-//               "INSERT INTO users (username, email, hashedPassword, created_at) VALUES (?, ?, ?, ?)";
-//             db.query(
-//               sqlquery,
-//               [username, email, hashedPassword, created_at],
-//               (err, result) => {
-//                 if (err) {
-//                   next(err);
-//                 }
-//                 res.send(
-//                   "User registered successfully! <a href=" + "../" + ">Home</a>"
-//                 );
-//               }
-//             );
-//           });
-//         }
-//       });
-//     }
-//   );
 
 router.get("/register", (req, res) => {
     res.render("register.ejs");
@@ -108,11 +63,54 @@ router.post("/registered", [
 
 });
 
+router.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
 
+router.post("/loggedin", async (req, res) => {
+    let username = req.sanitize(req.body.username);
+    let password = req.sanitize(req.body.password);
 
+    try {
+        const user = await pool.query('SELECT * FROM public.users WHERE username = $1', [username]);
+        if (user.rows.length > 0) {
+            bcrypt.compare(password, user.rows[0].hashedpassword, (err, result) => {
+                if (result) {
+                    res.send("Login successful! <a href=" + "../" + ">Home</a>");
+                } else {
+                    res.send("Incorrect password");
+                }
+            });
+        } else {
+            res.send("User does not exist");
+        }
+    } catch (err) {
+        console.error('Error logging in:', err.message);
+        res.status(500).send('Database query failed');
+    }
+});
 
+router.get("/logout", (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect("./");
+        }
+        res.clearCookie("sid");
+        res.redirect("./");
+    });
+});
 
+router.get("/profile", redirectLogin, (req, res) => {
+    res.send("Profile page");
+});
 
+router.get("edit", redirectLogin, (req, res) => {
+    const { userId } = req.session;
+
+    const user = pool.query('SELECT * FROM public.users WHERE id = $1', [userId]);
+    res.render("edit.ejs", { user });
+}
+);
 
 
 
